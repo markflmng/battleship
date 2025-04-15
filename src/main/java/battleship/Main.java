28e63e055895e6ff32a61a459f3357437244ad98
markflmng <mark.flaming@edu.fhdw.de>
2025-04-15 17:31:32 +0200

    Initial commit

---
+++ src/main/java/battleship/Main.java
@@ -0,0 +1,12 @@
+package battleship;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hier entsteht das Spiel Schiffe versenken!");
+
+
+    }
+
+    }
+
